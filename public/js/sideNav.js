85e340987cb0be4f468ec0f03c0abb35f9251a53
Li Yichang <yichang.li@autodesk.com>
2015-05-21 17:11:11 +0800

    tab tip has been added to side nav bar

--- public/js/sideNav.js
+++ public/js/sideNav.js
@@ -1,62 +1,90 @@
 $(document).ready(function(){
-    var collapsed = true;
+
+
+/*    some parameters that can be changed
+*       expand_duration: time to collapse or expand the side nav bar
+*       delay:           delay of tab-tip when mouse hover
+*
+* */
+
     var expand_duration = 500;
-    $('.header').click(function(){
+    var delay = 300;
+
+
+//function for collapse or expand the side nav bar
+    var collapsed = true;
+    $(".header").click(function(){
         if(collapsed){
             //side nav bar expand
-            $('#sidebar').animate({'width':'200px'},expand_duration,'swing');
-
-            $('.expand-icon').rotate(180,expand_duration);
+            $("#sidebar").animate({"width":"200px"},expand_duration,"swing");
+            $(".expand-icon").rotate(180,expand_duration);
             collapsed = false;
         }else{
             //side nav bar collapse
-            $('#sidebar').animate({'width':'50px'},expand_duration,'swing');
-
-            $('.expand-icon').rotate(0,expand_duration);
+            $("#sidebar").animate({"width":"50px"},expand_duration,"swing");
+            $(".expand-icon").rotate(0,expand_duration);
             collapsed = true;
         }
     });
-    $('a').click(function(){
-        if(collapsed){
-            $('a.active').parent().css('width','100%');
-            $('a.active').removeClass('active');
-            $(this).addClass('active');
 
+
+//    switch between active tab
+    $("#sidebar a").click(function(){
+        if(collapsed){
+            $("#sidebar .active").removeClass("active");
+            $(this).addClass("active");
         }else{
-            $('a.active').parent().css('width','100%');
-            $('a.active').removeClass('active');
-            $(this).addClass('active');
+            $("a.active").removeClass("active");
+            $(this).addClass("active");
         }
     });
-// tab tip on mouse hover with a delay of 1000ms
-    var timer;
-    var delay = 1000;
 
-    $('#sidebar ul li').hover(function() {
-        // on mouse in, start a timeout
 
-        timer = setTimeout(function() {
-            $()
-        }, delay);
-    }, function() {
-        // on mouse out, cancel the timer
-        clearTimeout(timer);
-    });
+// tab tip on mouse hover with delay
+
+    var timeout;
+    $("#sidebar a").hover(
+
+        function() {
+            var ob = $(this);
+            var b_color;
+            if(ob.hasClass("active")){
+                b_color = "#ff9800";
+            }else{
+                b_color = "#424242";
+            }
+            timeout = setTimeout(function(){
+                console.log(b_color);
+                ob.animate({"width":"300%","background-color":b_color});
+            }, delay);
+        },
+        function(){
+            var ob = $(this);
+            clearTimeout(timeout);
+            ob.animate({"width":'100%'});
+        }
+    );
+
 
 });
 
 
-//an improved version on rotate function
-//add duration parameter
+/*
+*   an improved version on rotate function
+*   add duration parameter
+*   for degrees: 0deg means original orientation
+*                90deg means rotate 90 degrees clockwise related to the original orientation, not current orientation
+*/
+
 jQuery.fn.rotate = function(degrees,duration) {
-    $(this).css({'-webkit-transform' : 'rotate('+ degrees +'deg)',
-        '-moz-transform' : 'rotate('+ degrees +'deg)',
-        '-ms-transform' : 'rotate('+ degrees +'deg)',
-        'transform' : 'rotate('+ degrees +'deg)',
-        'transition': duration+'ms',
-        '-moz-transition': duration+'ms',
-        '-webkit-transition': duration+'ms',
-        'ms-transition': duration+'ms'
+    $(this).css({'-webkit-transform' : "rotate("+ degrees +"deg)",
+        '-moz-transform' : "rotate("+ degrees +"deg)",
+        '-ms-transform' : "rotate("+ degrees +"deg)",
+        'transform' : "rotate("+ degrees +"deg)",
+        'transition': duration+"ms",
+        '-moz-transition': duration+"ms",
+        '-webkit-transition': duration+"ms",
+        'ms-transition': duration+"ms"
 
 });
     return $(this);
